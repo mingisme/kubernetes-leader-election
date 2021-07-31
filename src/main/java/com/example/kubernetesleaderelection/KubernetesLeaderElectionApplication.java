6805a336dff01ef32510780dfdbfcce07dba7c28
ming.wang4 <ming.wang4@envision-digital.com>
2021-08-01 00:32:59 +0800

    init

---
+++ src/main/java/com/example/kubernetesleaderelection/KubernetesLeaderElectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesleaderelection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesLeaderElectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesLeaderElectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kubernetesleaderelection/KubernetesLeaderElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesleaderelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesLeaderElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
